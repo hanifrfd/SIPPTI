524e210391c8fad11594c9de0470194f8e591b58
hanifrfd <hanif.wrong@gmail.com>
2018-10-01 12:17:23 +0700

    ubah base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/CI/Skripsi/';
+$config['base_url'] = 'http://localhost/Github/SIPPTI/';
 
 /*
 |--------------------------------------------------------------------------
